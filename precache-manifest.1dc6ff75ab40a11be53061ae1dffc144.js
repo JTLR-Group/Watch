65799b1601be2e362a98210a6390dff568d24a94
Jaiveer Talwar <jaiveer_talwar@giftsthatgive.in>
2021-10-11 08:24:01 +0530

    nice
    added

---
+++ precache-manifest.1dc6ff75ab40a11be53061ae1dffc144.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f26b3b106ebbcdb45ed405856a2047c",
+    "url": "/Simple_Is_Best/index.html"
+  },
+  {
+    "revision": "71d3935ff70864713346",
+    "url": "/Simple_Is_Best/static/css/main.af29c076.chunk.css"
+  },
+  {
+    "revision": "ab34c17eb5c4456a2b0c",
+    "url": "/Simple_Is_Best/static/js/2.80c1e2bb.chunk.js"
+  },
+  {
+    "revision": "71d3935ff70864713346",
+    "url": "/Simple_Is_Best/static/js/main.10716d26.chunk.js"
+  },
+  {
+    "revision": "025f2a2ef7e740ae0fc2",
+    "url": "/Simple_Is_Best/static/js/runtime~main.fe24ad13.js"
+  },
+  {
+    "revision": "2c9683e0a770a07fdaad3bc735daee23",
+    "url": "/Simple_Is_Best/static/media/spinner-2.2c9683e0.gif"
+  }
+]);
